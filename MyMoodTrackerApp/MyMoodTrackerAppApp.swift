a6a379ec54c4448725b300c9b2ebd42cf61e21a8
anuththara <it21232640@my.sliit.lk>
2024-11-23 17:43:43 +0530

    Initial Commit

---
+++ MyMoodTrackerApp/MyMoodTrackerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMoodTrackerAppApp.swift
+//  MyMoodTrackerApp
+//
+//  Created by Anuththara Divarathna on 2024-11-23.
+//
+
+import SwiftUI
+
+@main
+struct MyMoodTrackerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
